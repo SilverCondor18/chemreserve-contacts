94aea3cf537c2f65f739c0aa8c101705be236f0e
Maksym Dzyubenko <ckraer@gmail.com>
2024-12-19 20:57:05 +0200

    return partials

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-/*const pageHeader = document.querySelector('.page-header');
+const pageHeader = document.querySelector('.page-header');
 const pageFooter = document.querySelector('.page-footer');
 const pageBackdrop = document.querySelector('.modal-backdrop');
 pageHeader.innerHTML = await fetch('../partials/header.html').then(res => res.text());
 pageFooter.innerHTML = await fetch('../partials/footer.html').then(res => res.text());
 pageBackdrop.innerHTML = await fetch('../partials/burger-backdrop.html').then(res => res.text());
-*/
+
 const burgerButton = document.querySelector('.burger-button');
 const burgerClose = document.querySelector('.burger-close');
 const backdrop = document.querySelector('.modal-backdrop');
